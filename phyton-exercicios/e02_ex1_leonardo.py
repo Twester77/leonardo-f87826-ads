d89163d33bf315a7d681907a2d41258a29e70869
Leonardo F <87826@unifev.edu.br>
2026-08-10 20:07:01 -0300

    alguns exercicios basicos de python

---
+++ phyton-exercicios/e02_ex1_leonardo.py
@@ -0,0 +1,21 @@
+print("Olá, seja bem vindo!");
+numero = []
+a = int(input("Digite a primeira nota: "));
+numero.append(a)
+b = int(input("Digite a segunda nota: "));
+numero.append(b)
+c = int(input("Digite a terceira nota: "));
+numero.append(c)
+d = int(input("Digite a quarta nota: "));
+numero.append(d)
+e = int(input("Digite a quinta nota: "));
+numero.append(e)
+
+
+
+for i in range(0, 1):
+
+
+ print(f"A soma de a + b + c + d + e é: {a + b + c + d + e}");
+ print(f"A média de a + b + c + d + e é: {(a + b + c + d + e) / 5}");
+ 
